7f7cfba733c76824a2c3ceccb994c78f76ad876c
Ionica Bizau <bizauionica@yahoo.com>
2013-02-03 12:44:43 +0200

    Minor changes.

--- main.js
+++ main.js
@@ -89,6 +89,7 @@ define([
                     location.reload();
                 }
                 else {
+                    $("#pass").val("");
                     alert("Wrong password.");
                 }
             });
